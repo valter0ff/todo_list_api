b53b3b4abeee507246e950b66735903e915abd37
valerii.pohrebniak <km270985pvg@gmail.com>
2022-07-15 16:36:39 +0300

    configure redis

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -2,4 +2,4 @@
 
 JWTSessions.algorithm = 'HS256'
 JWTSessions.encryption_key = Rails.application.credentials.secret_jwt_encryption_key
-JWTSessions.token_store = :memory
+JWTSessions.token_store = :redis, { redis_client: Redis.current }
---
+++ config/initializers/redis.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+Redis.current = Redis.new(host: '127.0.0.1', port: 6379)
