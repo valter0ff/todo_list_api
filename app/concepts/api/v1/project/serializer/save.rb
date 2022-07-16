21d7a51bba55ed2a9d89fa4f1ce1197945505a12
valerii.pohrebniak <km270985pvg@gmail.com>
2022-07-17 01:21:36 +0300

    add Project contract and operation specs

--- app/concepts/api/v1/project/serializer/save.rb
+++ app/concepts/api/v1/project/serializer/save.rb
@@ -5,5 +5,6 @@ module Api::V1::Project::Serializer
     attributes :title, :created_at, :updated_at
 
     set_type :project
+    belongs_to :user, serializer: Api::V1::User::Serializer::Create
   end
 end
