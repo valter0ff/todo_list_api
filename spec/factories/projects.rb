d12648b8efc0fbb7639644924d153a1d43bfc924
valerii.pohrebniak <km270985pvg@gmail.com>
2022-07-17 21:59:24 +0300

    update routes, updare project factory

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
   factory :project do
     title { FFaker::Name.unique.name }
     user
+
+    trait :with_tasks do
+      tasks { [association(:task), association(:task)] }
+    end
   end
 end
