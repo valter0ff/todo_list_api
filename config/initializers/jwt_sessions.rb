2c2122b3f4f871880a35081568ef7ac8cf7acfbc
valerii.pohrebniak <km270985pvg@gmail.com>
2022-07-17 22:47:52 +0300

    Update JWT initializer

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 JWTSessions.algorithm = 'HS256'
-JWTSessions.encryption_key = Rails.application.credentials.secret_jwt_encryption_key
-JWTSessions.token_store = :redis, { redis_client: Redis.current }
+JWTSessions.encryption_key = ENV['JWT_KEY'] || Rails.application.credentials.secret_jwt_encryption_key
+
+if Rails.env.test?
+  JWTSessions.token_store = :memory
+elsif Rails.env.development?
+  JWTSessions.token_store = :redis, { redis_url: 'redis://127.0.0.1:6379/0' }
+end
