c7db8545c237894c6c404e804ac355dc0298cdc3
valerii.pohrebniak <km270985pvg@gmail.com>
2022-07-17 23:05:31 +0300

    add Project update endpoint with specs

--- app/concepts/api/v1/project/operation/update.rb
+++ app/concepts/api/v1/project/operation/update.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 module Api::V1::Project::Operation
-  class Create < ApplicationOperation
-    step :build_project
+  class Update < ApplicationOperation
+    step :set_project
+    fail Macro::Semantic(failure: :not_found), fail_fast: true
     step Contract::Build(constant: Api::V1::Project::Contract::Save)
     step Contract::Validate(key: :project)
     step Contract::Persist()
     fail Macro::ContractErrors(error: I18n.t('errors.unprocessable'))
     pass :set_serializer
-    pass Macro::Semantic(success: :created)
+    pass Macro::Semantic(success: :ok)
 
-    def build_project(ctx, current_user:, **)
-      ctx[:model] = current_user.projects.build
+    def set_project(ctx, params:, current_user:, **)
+      ctx[:model] = current_user.projects.find_by(id: params[:id])
     end
 
     def set_serializer(ctx, model:, **)
--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -4,4 +4,8 @@ class Api::V1::ProjectsController < AuthorizedController
   def create
     endpoint operation: Api::V1::Project::Operation::Create
   end
+
+  def update
+    endpoint operation: Api::V1::Project::Operation::Update
+  end
 end
