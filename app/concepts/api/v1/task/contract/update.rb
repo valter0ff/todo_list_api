b9802fb477e595ff171a26c68f4086fc93863bc7
valerii.pohrebniak <km270985pvg@gmail.com>
2022-07-18 13:10:41 +0300

    add task update endpoint, operation, contract

---
+++ app/concepts/api/v1/task/contract/update.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Api::V1::Task::Contract
+  class Update < ApplicationContract
+    property :name
+
+    validation :status do
+      configure do
+        config.namespace = :task
+        
+        def active?(value)
+          value == 'active'
+        end
+      end
+
+      required(:status).value(active?: :status)
+    end
+    
+    validation :default, if: :status do
+      configure { config.namespace = :task }
+
+      required(:name).filled(:str?)
+    end
+  end
+end 
---
+++ app/concepts/api/v1/task/operation/update.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Api::V1::Task::Operation
+  class Update < ApplicationOperation
+    step :set_model
+    fail Macro::Semantic(failure: :not_found), fail_fast: true
+    step Contract::Build(constant: Api::V1::Task::Contract::Update)
+    step Contract::Validate(key: :task)
+    step Contract::Persist()
+    fail Macro::ContractErrors(error: I18n.t('errors.unprocessable'))
+    pass :set_serializer
+    pass Macro::Semantic(success: :ok)
+
+    def set_model(ctx, current_user:, params:, **)
+      ctx[:model] = current_user.tasks.find_by(id: params[:id])
+    end
+
+    def set_serializer(ctx, model:, **)
+      ctx[:serializer] = Api::V1::Task::Serializer::Show.new(model)
+    end
+  end
+end 
